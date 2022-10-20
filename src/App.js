5a8177d199a667e4a3ede5f9ccfc6a8ef1f27c21
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-10-20 17:37:25 -0400

    snapshot

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from './components/About';
 import Gallery from './components/Gallery';
 
 function App() {
-
   const [categories] = useState([
     {
       name: 'commercial',
@@ -13,7 +12,7 @@ function App() {
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
+  ]); 
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
@@ -22,10 +21,11 @@ function App() {
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory} />
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-      <Gallery currentCategory={currentCategory}></Gallery>
-        <About />
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
