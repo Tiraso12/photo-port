0db1bab441239d260f4f7ca26f72d368c051f270
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-10-24 17:20:41 -0400

    fixed bug

--- src/components/Modal/__tests__/index.test.js
+++ src/components/Modal/__tests__/index.test.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { render, cleanup, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Modal from '../../Modal';
+// __tests__/Modal.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
 
 const mockToggleModal = jest.fn();
 const currentPhoto = {
@@ -11,35 +12,34 @@ const currentPhoto = {
   index: 1
 };
 
-
 afterEach(cleanup);
- 
-describe('Modal component', ()=>{
-    it('renders', ()=>{
-        render(<Modal
-        onClose={mockToggleModal}
-        currentPhoto={currentPhoto}/>);
-    });
 
-    it('matches snapshot', ()=>{
-        const { asFragment} = render(<Modal
-            onClose={mockToggleModal}
-            currentPhoto={currentPhoto}/>);
-
-            expect(asFragment()).toMatchSnapshot();
-    });
-    
+describe('Modal component', () => {
+  it('renders', () => {
+    render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
   })
 
-  describe('Click Event', () => {
-    it('calls onClose handler', () => {
-      const { getByText } = render(<Modal
-        onClose={mockToggleModal}
-        currentPhoto={currentPhoto}
-      />)
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
 
-      fireEvent.click(getByText('Close this modal'))
-  
-      expect(mockToggleModal).toHaveBeenCalledTimes(1);
-    });
-  }) 
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+describe('Click Event', () => {
+  it('calls onClose handler', () => {
+    const { getByText } = render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
+    fireEvent.click(getByText('Close this modal'))
+
+    expect(mockToggleModal).toHaveBeenCalledTimes(1);
+  });
+})  
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -4,6 +4,7 @@ import Modal from '../Modal';
 const PhotoList = ({ category }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPhoto, setCurrentPhoto] = useState();
+
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -126,14 +127,17 @@ const PhotoList = ({ category }) => {
   const toggleModal = (image, i) => {
     setCurrentPhoto({ ...image, index: i });
     setIsModalOpen(!isModalOpen);
-  }
+  };
+
   return (
-    <div>{isModalOpen && (<Modal currentPhoto={currentPhoto} onClose={toggleModal} />
-    )}
+    <div>
+      {isModalOpen && (
+        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
             onClick={() => toggleModal(image, i)}
